ec2eea715887868e1352f14ee54df1f533ab8440
maurizio <mauriziodm91@gmail.com>
2022-02-03 20:07:16 -0300

    modified model and added planets source

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,18 @@
 const http = require('http')
+const { start } = require('repl')
 const app = require('./app')
+const { loadPlanetsData } = require('./models/planets.model')
 require('dotenv').config()
 
 const PORT = process.env.PORT
 
 const server = http.createServer(app)
-server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`)
-})
+
+async function startServer() {
+  await loadPlanetsData()
+  server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+  })
+}
+
+startServer()
