f13150a954a7d8b1e7e055327b4d4225d944c0d8
maurizio <mauriziodm91@gmail.com>
2022-02-03 17:53:56 -0300

    initial commit

---
+++ client/src/hooks/requests.js
@@ -0,0 +1,21 @@
+async function httpGetPlanets() {
+  const response = await fetch('http://localhost:8000/planets')
+  return await response.json()
+}
+
+async function httpGetLaunches() {
+  // TODO: Once API is ready.
+  // Load launches, sort by flight number, and return as JSON.
+}
+
+async function httpSubmitLaunch(launch) {
+  // TODO: Once API is ready.
+  // Submit given launch data to launch system.
+}
+
+async function httpAbortLaunch(id) {
+  // TODO: Once API is ready.
+  // Delete launch with given ID.
+}
+
+export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch }
