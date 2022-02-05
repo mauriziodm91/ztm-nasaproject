456479ad624d7438721eb6cd54191b3db6f53c00
maurizio <mauriziodm91@gmail.com>
2022-02-04 23:42:36 -0300

    added log and launches model

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,22 @@
+const path = require('path')
 const express = require('express')
 const planetsRouter = require('./routes/planets/planets.router')
 const app = express()
 const cors = require('cors')
+const morgan = require('morgan')
 
 app.use(
   cors({
     origin: 'http://localhost:3000',
   })
 )
+app.use(morgan('combined'))
 app.use(express.json())
+//Serving the frontend folder
+app.use(express.static(path.join(__dirname, '..', 'public')))
 app.use(planetsRouter)
+//Assingnin index.html to the root path
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+})
 module.exports = app
--- server/src/routes/planets/planets.controllers.js
+++ server/src/routes/planets/planets.controllers.js
@@ -1,7 +1,6 @@
 const { planets } = require('../../models/planets.model')
 
 function getAllPlanets(req, res) {
-  console.log('req received')
   res.status(200).json(planets)
 }
 module.exports = {
