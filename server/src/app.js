5dc17e6a7654f921efbc54b227ec1639a0de5fc3
maurizio <mauriziodm91@gmail.com>
2022-02-06 18:28:39 -0300

    created launches router and controller files to serve in the frontend

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const express = require('express')
 const planetsRouter = require('./routes/planets/planets.router')
+const launchesRouter = require('./routes/launches/launches.router')
 const app = express()
 const cors = require('cors')
 const morgan = require('morgan')
@@ -14,8 +15,10 @@ app.use(morgan('combined'))
 app.use(express.json())
 //Serving the frontend folder
 app.use(express.static(path.join(__dirname, '..', 'public')))
+//Routers
 app.use(planetsRouter)
-//Assingnin index.html to the root path
+app.use(launchesRouter)
+//Assingning index.html to the root path
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
 })
