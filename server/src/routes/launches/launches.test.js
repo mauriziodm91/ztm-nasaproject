24d08d22c7a62e4b160688a0382c5ef23b932ddb
maurizio <mauriziodm91@gmail.com>
2022-02-13 17:18:10 -0300

    created launches test file and installed jest

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,70 @@
+const request = require('supertest')
+const app = require('../../app')
+
+describe('Test GET /launches ', () => {
+  test('It should response with 200 success', async () => {
+    const response = await request(app).get('/launches')
+    expect(200)
+  })
+})
+
+describe('Test POST /launches', () => {
+  const completeLaunchData = {
+    mission: 'USS ENTREPRISE',
+    rocket: 'NCC 1701-D',
+    target: 'Kepler-186 f',
+    launchDate: 'January 4, 2028',
+  }
+
+  const launchDataWithoutDate = {
+    mission: 'USS ENTREPRISE',
+    rocket: 'NCC 1701-D',
+    target: 'Kepler-186 f',
+  }
+
+  const launchDataWithInvalidDate = {
+    mission: 'USS ENTREPRISE',
+    rocket: 'NCC 1701-D',
+    target: 'Kepler-186 f',
+    launchDate: 'hi',
+  }
+
+  test('it should respond with 201 created', async () => {
+    const response = await request(app)
+      .post('/launches')
+      .send(completeLaunchData)
+      .expect('Content-type', /json/)
+      .expect(201)
+
+    const requestDate = new Date(completeLaunchData.launchDate).valueOf()
+    const responseDate = new Date(response.body.launchDate).valueOf()
+
+    expect(responseDate).toBe(requestDate)
+
+    expect(response.body).toMatchObject(launchDataWithoutDate)
+  })
+
+  test('It should catch missing required properties', async () => {
+    const response = await request(app)
+      .post('/launches')
+      .send(launchDataWithoutDate)
+      .expect('Content-Type', /json/)
+      .expect(400)
+
+    expect(response.body).toStrictEqual({
+      error: 'Missing required launch property',
+    })
+  })
+
+  test('It should check for invalid date', async () => {
+    const response = await request(app)
+      .post('/launches')
+      .send(launchDataWithInvalidDate)
+      .expect('Content-Type', /json/)
+      .expect(400)
+
+    expect(response.body).toStrictEqual({
+      error: 'Invalid launch date',
+    })
+  })
+})
